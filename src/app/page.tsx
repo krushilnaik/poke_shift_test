f71287b0bec7c59c595b0e1f121681606d70f0df
Krushil <krushilnaik96@gmail.com>
2023-09-20 11:07:05 -0400

    Add pokeball loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { motion, Variants, AnimatePresence } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Pokemon } from "@/types";
+import pokeball from "@/json/pokeball.json";
 
 const typeColors = {
   Normal: "#A8A77A",
@@ -39,10 +40,16 @@ export default function Home() {
     animate: {
       opacity: 1,
       scale: 1,
+      transition: {
+        duration: 0.3,
+      },
     },
     exit: {
       opacity: 0,
-      scale: 0
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
     },
   };
 
@@ -52,10 +59,6 @@ export default function Home() {
       .then(setData)
       .then(() => setActive(493));
   }, []);
-
-  if (!data.length) {
-    return <div>Loading</div>;
-  }
 
   return (
     <main className="relative grid place-content-center w-full h-screen overflow-clip gap-6">
@@ -78,46 +81,59 @@ export default function Home() {
             className="relative rounded-lg w-[570px] max-w-[90vw] aspect-square z-10"
           >
             <AnimatePresence mode="popLayout">
-              {data[active - 1].paths.map((tri, i) => (
-                <motion.path
-                  layout
-                  d={tri.points}
-                  color={tri.color}
-                  variants={variants}
-                  initial="initial"
-                  animate="animate"
-                  transition={{ duration: 0.5 }}
-                  exit="exit"
-                  key={`tri-${i}`}
-                  className="duration-[850ms]"
-                />
-              ))}
+              {data.length
+                ? data[active - 1].paths.map((tri, i) => (
+                    <motion.path
+                      layout
+                      d={tri.points}
+                      color={tri.color}
+                      variants={variants}
+                      initial="initial"
+                      animate="animate"
+                      transition={{ duration: 0.5 }}
+                      exit="exit"
+                      key={`tri-${i}`}
+                      className="duration-[850ms]"
+                    />
+                  ))
+                : pokeball.paths.map((tri, i) => (
+                    <motion.path
+                      layout
+                      d={tri.points}
+                      color={tri.color}
+                      transition={{ duration: 0.5 }}
+                      key={`tri-${i}`}
+                      className="animate-pulse [animation-duration:0.9s]"
+                    />
+                  ))}
             </AnimatePresence>
           </svg>
 
           <AnimatePresence mode="wait">
-            <motion.div
-              className="flex flex-col gap-4 items-center"
-              layout
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ x: -100, opacity: 0 }}
-              transition={{ duration: 0.2 }}
-              key={`${data[active - 1].name}`}
-            >
-              <h1 className="text-5xl">{data[active - 1].name}</h1>
-              <ul className="flex flex-row gap-3">
-                {data[active - 1].types.map((t, i) => (
-                  <li
-                    key={`${data[active - 1].name}-type-${i}`}
-                    className="px-2 py-1 rounded-md"
-                    style={{ backgroundColor: typeColors[t] }}
-                  >
-                    {t}
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
+            {data.length && (
+              <motion.div
+                className="flex flex-col gap-4 items-center"
+                layout
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -100, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                key={`${data[active - 1].name}`}
+              >
+                <h1 className="text-5xl">{data[active - 1].name}</h1>
+                <ul className="flex flex-row gap-3">
+                  {data[active - 1].types.map((t, i) => (
+                    <li
+                      key={`${data[active - 1].name}-type-${i}`}
+                      className="px-2 py-1 rounded-md"
+                      style={{ backgroundColor: typeColors[t] }}
+                    >
+                      {t}
+                    </li>
+                  ))}
+                </ul>
+              </motion.div>
+            )}
           </AnimatePresence>
         </div>
 
@@ -130,7 +146,7 @@ export default function Home() {
           <FontAwesomeIcon icon={faChevronRight} />
         </button>
       </div>
-      <div className="absolute flex w-full justify-between bottom-6 p-5 md:hidden">
+      <div className="absolute flex w-full justify-between bottom-6 p-10 md:hidden">
         <button
           role="navigation"
           onClick={() => setActive(active - 1)}
